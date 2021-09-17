446af5a1ab472fe99c1af07a36a5f86cf94e2e53
Mansour Ashraf <mansourashraf173@gmail.com>
2021-09-17 22:21:22 +0200

    Chap04 is Done

--- Ch04/04_01/app.py
+++ Ch04/04_01/app.py
@@ -1,2 +1,13 @@
-value = input('Enter a number: ')
+# input() method Takes the input as a string
+# int() method converts any data type to intger
+# to can use it in arthimatric operations
+
+
+
+value = int(input('Enter a number: '))
+
 print(value)
+
+print(value, " Is my fav Number")
+
+print(value * 10, " Is my fav number 10 Times =D")
