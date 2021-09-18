5c6d11a059409603f0d8e917ea238fc3b287b8d5
Mansour Ashraf <mansourashraf173@gmail.com>
2021-09-18 02:03:51 +0200

    Done Chap07

--- Ch07/07_04 count.py
+++ Ch07/07_04 count.py
@@ -1,4 +1,4 @@
 i = 10
-while i < 0:
+while i > 0:
     i -= 1
     print(i) 
--- Ch07/07_06 plant.py
+++ Ch07/07_06 plant.py
@@ -1,12 +1,15 @@
 def plant_recommendation(care):
-    if care = 'low':
+    # Syntax Error
+    if care == 'low':
         print('aloe')
     elif care == 'medium':
         print('pothos')
-    elif care == 'medium':
+    # Logic Error
+    elif care == 'high':
         print('orchid')
 
-plant_rec('low')
+# Runtime Error
+plant_recommendation('low')
 plant_recommendation('medium')
 plant_recommendation('high')
 
